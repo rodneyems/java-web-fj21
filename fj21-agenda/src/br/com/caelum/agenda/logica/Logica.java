08e2e9a3ef5eb59c94da813f0ffaf3721e54838f
Rodney Martins <rodney.martins@live.com>
2021-07-05 22:31:38 -0300

    dia 6 - configurando Spring

--- fj21-agenda/src/br/com/caelum/agenda/logica/Logica.java
+++ fj21-agenda/src/br/com/caelum/agenda/logica/Logica.java
@@ -1,10 +1,12 @@
 package br.com.caelum.agenda.logica;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Logica {
 	
-	public String executa(HttpServletRequest request, HttpServletResponse response);
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws SQLException;
 
 }
